cc48dcdc1f72bbbf0a1c334aaf306a5e6bcfa28b
Ahmad Zacky <zackfxg@gmail.com>
2023-03-06 12:49:53 +0700

    fixed rusdy

--- src/navigation/AuthNavigator.js
+++ src/navigation/AuthNavigator.js
@@ -1,17 +1,17 @@
-import react from 'react';
-import { createStackNavigator } from "@react-navigation/stack";
-import {LoginScreen} from '../screens/auth';
-import {ROUTES} from '../constant';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import LoginScreen from '../screens/auth/LoginScreen';
+import ROUTES from '../constant/route';
 
 const Stack = createStackNavigator();
 
 function AuthNavigator() {
-    console.log(Stack);
-    return(
-        <Stack.Navigator>
-            <Stack.Screen name={ROUTES.LOGIN} component={LoginScreen} />
-        </Stack.Navigator>
-    );
+  console.log(Stack);
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={ROUTES.LOGIN} component={LoginScreen} />
+    </Stack.Navigator>
+  );
 }
 
 export default AuthNavigator;
